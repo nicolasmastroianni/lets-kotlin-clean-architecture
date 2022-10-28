1b2e72ed8445138fbfcf78662a12433e42bd7c0b
Nico <nmastroianni@serv.externos.prismamp.com>
2022-10-28 17:14:36 -0300

    feat(seed): add rest client example

---
+++ src/main/kotlin/org/redbee/adapter/out/rest/PokemonRestAdapter.kt
@@ -0,0 +1,28 @@
+package org.redbee.adapter.out.rest
+
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient
+import org.redbee.adapter.out.rest.model.PokemonRestModel
+import org.redbee.application.port.out.PokemonRepository
+import org.redbee.application.usecase.model.Pokemon
+import org.slf4j.LoggerFactory
+import javax.enterprise.context.ApplicationScoped
+import javax.ws.rs.client.Client
+import javax.ws.rs.client.ClientBuilder
+import javax.ws.rs.core.GenericType
+
+@ApplicationScoped
+@RegisterRestClient
+class PokemonRestAdapter() : PokemonRepository{
+    private val log = LoggerFactory.getLogger(this::class.java)
+
+    override fun get(name: String): Pokemon {
+        log.info("Ejecutando caso de uso de obtener pokemon con nombre:$name")
+        val client: Client = ClientBuilder.newBuilder().build()
+        val url = "https://pokeapi.co/api/v2/pokemon/${name}"
+        val pokemonResult: PokemonRestModel = client.target(url).request().get<PokemonRestModel>(object : GenericType<PokemonRestModel?>() {})
+        val pokemon = pokemonResult.toDomain()
+        log.info("Ejecutando caso de uso de obtener pokemon con nombre:$name")
+
+        return pokemon
+    }
+}
---
+++ src/main/kotlin/org/redbee/adapter/out/rest/model/AbilityInformationRestModel.kt
@@ -0,0 +1,16 @@
+package org.redbee.adapter.out.rest.model
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+import io.quarkus.runtime.annotations.RegisterForReflection
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@RegisterForReflection
+data class AbilityInformationRestModel(
+    var ability : AbilityRestModel = AbilityRestModel("")
+) {
+}
