39eeeefe67b8391388b6587c90e04db14eab151c
Nico <nmastroianni@serv.externos.prismamp.com>
2022-10-28 17:16:46 -0300

    feat(seed): refactor logs

--- src/main/kotlin/org/redbee/adapter/out/rest/PokemonRestAdapter.kt
+++ src/main/kotlin/org/redbee/adapter/out/rest/PokemonRestAdapter.kt
@@ -16,12 +16,12 @@ class PokemonRestAdapter() : PokemonRepository{
     private val log = LoggerFactory.getLogger(this::class.java)
 
     override fun get(name: String): Pokemon {
-        log.info("Ejecutando caso de uso de obtener pokemon con nombre:$name")
+        log.info("Obteniendo pokemon con nombre:$name")
         val client: Client = ClientBuilder.newBuilder().build()
         val url = "https://pokeapi.co/api/v2/pokemon/${name}"
         val pokemonResult: PokemonRestModel = client.target(url).request().get<PokemonRestModel>(object : GenericType<PokemonRestModel?>() {})
         val pokemon = pokemonResult.toDomain()
-        log.info("Ejecutando caso de uso de obtener pokemon con nombre:$name")
+        log.info("Pokemon obtenido:${pokemon.toString()}")
 
         return pokemon
     }
